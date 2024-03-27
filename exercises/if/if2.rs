b4ef779ca4cbac47ffcfb1033637cf6ca5ec512b
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2024-03-27 16:05:45 -0300

    personal progress

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,13 +5,16 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// String ... vector<char>[10]; | char *a = (char *)malloc(10*sizeof(float));
+// &str ... char[10]; | char *
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // let a = [
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    // ];
+    let a = [0; 400];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
