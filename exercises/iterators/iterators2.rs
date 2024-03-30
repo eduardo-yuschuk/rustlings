35cddff693b599b755a45e9b88d0ee6440a2a574
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2024-03-30 16:48:52 -0300

    -

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -29,7 +27,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|x| capitalize_first(*x)).collect()
+    //vec![]
 }
 
 // Step 3.
@@ -37,7 +36,8 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    //String::new()
+    words.iter().map(|x| capitalize_first(*x)).collect()
 }
 
 #[cfg(test)]
